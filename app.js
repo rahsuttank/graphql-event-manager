ef59b340f9e94e913f74c3ffc76f4ab472485818
rahsuttank <tushar.tank24@gmail.com>
2025-02-10 22:21:38 -0600

    Added mock data

--- app.js
+++ app.js
@@ -7,18 +7,34 @@ const { buildSchema } = require('graphql');
 
 const app = express();
 
+const events = [];
+
 app.use(bodyParser.json());
 
 app.use('/graphqlApi', graphqlHTTP({
     schema: buildSchema(`
 
-        type RootQuery {
-            events: [String!]!
+        type Event {
+            _id: ID!
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
+        
+        input EventInput {
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
 
+        type RootQuery {
+            events: [Event!]!
         }
 
         type RootMutation { 
-            createEvent(name: String): String
+            createEvent(eventInput: EventInput): Event
         }
 
         schema {
@@ -28,11 +44,19 @@ app.use('/graphqlApi', graphqlHTTP({
         `),
     rootValue: {
         events: () => {
-            return ['Romantic', 'Coding', 'Sailing']
+            return events
         },
         createEvent: (args) => {
-            const eventName = args.name;
-            return eventName;
+            // console.log(args)
+            const event = {
+                _id: Math.random().toString(),
+                title: args.eventInput.title,
+                description: args.eventInput.description,
+                price: +args.eventInput.price,
+                date: new Date().toString()
+            }
+            events.push(event);
+            return event;
 
         }
     },
