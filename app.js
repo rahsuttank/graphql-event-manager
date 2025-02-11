8a1874698a6fa887f92cd02be6f222ebd6722806
rahsuttank <tushar.tank24@gmail.com>
2025-02-10 21:52:21 -0600

    Initial GraphQL setup

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { graphqlHTTP }  = require('express-graphql');
+const { buildSchema } = require('graphql');
+// const e = require('express');
+
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use('/graphqlApi', graphqlHTTP({
+    schema: buildSchema(`
+
+        type RootQuery {
+            events: [String!]!
+
+        }
+
+        type RootMutation { 
+            createEvent(name: String): String
+        }
+
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+        `),
+    rootValue: {
+        events: () => {
+            return ['Romantic', 'Coding', 'Sailing']
+        },
+        createEvent: (args) => {
+            const eventName = args.name;
+            return eventName;
+
+        }
+    },
+    graphiql: true
+}))
+
+app.listen(3000);
